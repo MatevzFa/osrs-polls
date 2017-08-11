d36e131f0e7c574b319e0d9ca851dc5a120dea97
matevzfa <mf6422@student.uni-lj.si>
2017-08-11 12:48:16 +0200

    Rewritten archive with db connection closing

--- archive.js
+++ archive.js
@@ -4,7 +4,6 @@
  *  update tables 'quesiton' and 'answer' to contain questions and possible answers
  *  get latest result for given poll_ID, and insert results into table 'votes',
     if the poll is currently live.
-    /// In that case, also update field 'votes' in table 'poll' ///
 */
 
 const request = require('request');
@@ -12,8 +11,6 @@ const cheerio = require('cheerio');
 const mysql = require('mysql');
 
 const dbParams = require('./db-params.json');
-const connection = mysql.createConnection(dbParams);
-connection.connect();
 
 const URL_ARCHIVE = 'http://services.runescape.com/m=poll/oldschool/archive.ws';
 const URL_BASE_ARCHIVE_BY_YEAR = 'http://services.runescape.com/m=poll/oldschool/archive.ws?year=';
@@ -23,13 +20,90 @@ var LIVE_POLL_ID = null;
 
 const INTERVAL = 5;
 
+function dbConnection(callback) {
+    let connection = mysql.createConnection(dbParams);
+    let cbPromise = new Promise((resolve, reject) => {
+        callback(connection, resolve, reject)
+    });
+
+    cbPromise
+        .then(() => connection.end())
+        .catch((err) => {
+            if (err.message) console.error(err.message);
+            else if (err.code) console.error(err.code);
+            else if (err) console.error(err);
+            connection.end();
+        });
+}
+
+function insertPolls(polls) {
+    dbConnection((connection, resolve, reject) => {
+
+        connection.query('INSERT INTO poll (poll_ID, title, date) VALUES ?', [polls], (err, results) => {
+            if (err && err.code !== 'ER_DUP_ENTRY') {
+                reject(err);
+            } else {
+                console.log('[archive] Inserted %d polls.', results.affectedRows);
+                resolve();
+            }
+        });
+    });
+}
+
+function insertQuestions(questions) {
+    return new Promise((insertResolve, insertReject) => {
+        dbConnection((connection, resolve, reject) => {
+
+            connection.query('INSERT INTO question (poll_ID, question_ID, question_text, question_type) VALUES ?', [questions], (err) => {
+                if (err && err.code !== 'ER_DUP_ENTRY') {
+                    reject(err);
+                    insertReject(err);
+                } else {
+                    resolve();
+                    insertResolve();
+                }
+            });
+        });
+    });
+}
+
+function insertAnswers(answers) {
+    return new Promise((insertResolve, insertReject) => {
+        dbConnection((connection, resolve, reject) => {
+
+            connection.query('INSERT INTO answer (poll_ID, question_ID, answer_ID, answer_text) VALUES ?', [answers], (err) => {
+                if (err && err.code !== 'ER_DUP_ENTRY') {
+                    reject(err);
+                    insertReject(err);
+                } else {
+                    resolve();
+                    insertResolve();
+                }
+            });
+        });
+    });
+}
+
+function insertVotes(votes) {
+
+    dbConnection((connection, resolve, reject) => {
+        connection.query('INSERT INTO votes (poll_ID, question_ID, answer_ID, amount, data_time) VALUES ?', [votes], (err) => {
+            if (err && err.code !== 'ER_DUP_ENTRY') {
+                reject(err);
+            } else {
+                resolve();
+            }
+        });
+    });
+}
+
 /**
  * Fetches and stores live poll id, if present
  */
 function getLivePollId() {
     return new Promise((resolve, reject) => {
         request('http://services.runescape.com/m=poll/oldschool/index.ws', (error, response, html) => {
-            if (!error) {   
+            if (!error) {
                 var $ = cheerio.load(html);
                 var id = $('div.current a').attr('href');
                 if (id) {
@@ -49,7 +123,7 @@ function getLivePollId() {
                     reject();
                 }
             } else {
-                console.log(error);
+                console.error(error);
                 reject();
             }
         });
@@ -68,14 +142,20 @@ function addLiveToArchive() {
             let date = dateStringToISO(fullTitle);
             let title = fullTitle.substring(0, fullTitle.length - date.length - 3);
             let poll = {
-                title: title,
                 poll_ID: LIVE_POLL_ID,
+                title: title,
                 date: date
             };
-            connection.query('INSERT INTO poll SET ?', poll, (error) => {
-                if (error && error.code !== 'ER_DUP_ENTRY') console.log(error.message);
-                else if (error && error.code === 'ER_DUP_ENTRY') console.log(`[archive] Live poll '${poll.poll_ID}' has already been archived`);
-                else if (!error) console.log(`[archive] Successful insert of live poll '${poll.poll_ID}'`);
+            dbConnection((connection, resolve, reject) => {
+                connection.query('INSERT INTO poll SET ?', poll, (err) => {
+                    if (err && err.code !== 'ER_DUP_ENTRY')
+                        reject(err);
+                    else if (!err) {
+                        console.log(`[archive] Successful insert of live poll '${poll.poll_ID}'`);
+                        resolve();
+                    } else
+                        resolve();
+                });
             });
         }
     });
@@ -86,41 +166,34 @@ function addLiveToArchive() {
  */
 function updatePollArchive() {
     console.log('[archive] Updating poll archive.');
+
     request(URL_ARCHIVE, (error, response, html) => {
         if (error)
             console.log(error);
         else {
-            var $ = cheerio.load(html);
-            const years = $('div.archiveYears p').text().match(/[0-9]+/g);
+            let $ = cheerio.load(html);
+            let years = $('div.archiveYears p').text().match(/[0-9]+/g);
             for (let i = 0; i < years.length; i++) {
                 request(URL_BASE_ARCHIVE_BY_YEAR + years[i], (error, response, html) => {
                     if (error)
                         console.log(error);
                     else {
-                        var $ = cheerio.load(html);
-                        var pollCount = $('.widescroll-content table tr').length;
+                        let $ = cheerio.load(html);
+                        let pollCount = $('.widescroll-content table tr').length;
+
+                        let polls = [];
 
                         for (let j = 0; j < pollCount; j++) {
-                            let poll = {
-                                title: $('.widescroll-content table tr .td80percent a').eq(j).text().replace('\uFFFD', '-'),
-                                poll_ID: $('.widescroll-content table tr .td80percent a').eq(j).attr('href').replace('results.ws?id=', ''),
-                                date: dateStringToISO($('.widescroll-content table tr .td20percent').eq(j).text())
-                            };
-                            connection.query('INSERT INTO poll SET ?', poll, (error) => {
-                                if (error && error.code !== 'ER_DUP_ENTRY')
-                                    console.log(error.message);
-                                else if (!error) {
-                                    console.log(`[archive] successful insert of poll '${poll.poll_ID}'`);
-                                    parsePollById(poll.poll_ID, false);
-                                }
-                            });
-                            connection.query(`UPDATE poll SET date = '${poll.date}' WHERE poll_ID = ${poll.poll_ID} AND date != '${poll.date}'`, (error, fields) => {
-                                if (error)
-                                    console.log(error.message);
-                                else if (fields.affectedRows != 0)
-                                    console.log(`[archive] successful update of date for poll '${poll.poll_ID}'`);
-                            });
+                            let poll = [
+                                $('.widescroll-content table tr .td80percent a').eq(j).attr('href').replace('results.ws?id=', ''), // id
+                                $('.widescroll-content table tr .td80percent a').eq(j).text().replace('\uFFFD', '-'), // title
+                                dateStringToISO($('.widescroll-content table tr .td20percent').eq(j).text()) // date
+                            ];
+
+                            polls.push(poll);
                         }
+
+                        insertPolls(polls);
                     }
                 });
             }
@@ -134,22 +207,32 @@ function updatePollArchive() {
  */
 function updateArchivedPolls() {
     console.log('[archive] Updating archived polls.');
-    connection.query(`SELECT * FROM poll WHERE poll_ID != ${(LIVE_POLL_ID ? LIVE_POLL_ID : 0)}`, (error, rows, fields) => {
-        if (error) console.log(error)
-        else {
-            for (let i = 0; i < rows.length; i++) {
-                setTimeout(function() {
-                    parsePollById(rows[i].poll_ID, false);
-                }, i * 200); 
+    let sql = `
+        SELECT poll_ID FROM poll
+        WHERE
+            poll_ID != ${(LIVE_POLL_ID ? LIVE_POLL_ID : 0)} AND
+            poll_ID NOT IN (SELECT DISTINCT poll_ID FROM votes);`;
+
+    dbConnection((connection, resolve, reject) => {
+        connection.query(sql, (err, rows, fields) => {
+            if (err)
+                reject(err);
+            else {
+                for (let i = 0; i < rows.length; i++) {
+                    setTimeout(function () {
+                        parsePollById(rows[i].poll_ID, false);
+                    }, i * 200);
+                }
+                resolve();
             }
-        }
+        });
     });
 }
 
 /**
  * Updates tables 'question' and 'answer' with questions and possible answers for given poll_ID
- * @param {*} poll_ID 
- * @param {*} isLive 
+ * @param {number} poll_ID 
+ * @param {boolean} isLive 
  */
 function parsePollById(poll_ID, isLive) {
 
@@ -164,9 +247,11 @@ function parsePollById(poll_ID, isLive) {
         }
 
         let date;
-        if (isLive)
+        if (isLive) {
             date = new Date(response.headers.date).toISOString();
-        else
+            console.log('my: %s              jagex: %s', (new Date()).toISOString(), date);
+            return;
+        } else
             date = null;
 
         if (error) console.log(error);
@@ -214,32 +299,15 @@ function parsePollById(poll_ID, isLive) {
                 }
             }
 
-            connection.query('INSERT INTO question (poll_ID, question_ID, question_text, question_type) VALUES ?', [questions], (error) => {
-                if (error && error.code !== 'ER_DUP_ENTRY')
-                    console.log(error);
-                else if (!error || error.code === 'ER_DUP_ENTRY') {
-
-                    if (error && error.code !== 'ER_DUP_ENTRY')
-                        console.log(`[archive] successful insert of questions for poll '${poll_ID}'`);
-
-                    connection.query('INSERT INTO answer (poll_ID, question_ID, answer_ID, answer_text) VALUES ?', [answers], (error) => {
-                        if (error && error.code !== 'ER_DUP_ENTRY')
-                            console.log(error);
-                        else if (!error || error.code === 'ER_DUP_ENTRY') {
-
-                            if (error && error.code !== 'ER_DUP_ENTRY')
-                                console.log(`[archive] successful insert of answers for poll '${poll_ID}'`);
-
-                            connection.query('INSERT INTO votes (poll_ID, question_ID, answer_ID, amount, data_time) VALUES ?', [votes], (error) => {
-                                if (error && error.code !== 'ER_DUP_ENTRY')
-                                    console.log(error);
-                                else if (!error)
-                                    console.log(`[archive]${(isLive ? ' [' + date.substring(0, 19) + '] ' : '')}successful insert of votes for poll '${poll_ID}'`);
-                            });
-                        }
-                    });
-                }
-            });
+            insertQuestions(questions)
+                .then(() => {
+                    insertAnswers(answers)
+                        .then(() => {
+                            insertVotes(votes);
+                        })
+                        .catch((err) => { });
+                })
+                .catch((err) => { });
         }
     });
 }
@@ -265,7 +333,9 @@ function dateStringToISO(dateString) {
 }
 
 /**
- * Calls callback function every 'minute' minutes (relative to XXh 00min 00s)
+ * 
+ * @param {number} minute 
+ * @callback callback
  */
 function runAtMinute(minute, callback) {
     let curMs = Date.now() % 3600000;
@@ -280,16 +350,20 @@ function startLivePollParser() {
     console.log('[archive] Starting live poll parser');
     runAtMinute(INTERVAL, () => {
         let livePromise = getLivePollId();
-        livePromise.then(() => {
-            parsePollById(LIVE_POLL_ID, true);
-        });
+        livePromise
+            .then(() => {
+                parsePollById(LIVE_POLL_ID, true);
+            })
+            .catch(() => { });
+        ;
     });
 }
 
 let startupActionSeq = [
     updatePollArchive,
+    updateArchivedPolls,
     startLivePollParser
 ]
 for (let i = 0; i < startupActionSeq.length; i++) {
-    setTimeout(startupActionSeq[i], i * 1000);
+    setTimeout(startupActionSeq[i], i * 5000);
 }
